f4883df4a1ab1fba1f591e1192a0a99599aa1329
Nguyễn Huỳnh Minh Tiến <33385777+Titytus@users.noreply.github.com>
2020-04-08 15:42:48 +0700

    gdi+

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -29,22 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.picOggy = new System.Windows.Forms.PictureBox();
             this.pnlInfo = new System.Windows.Forms.Panel();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.picOggy)).BeginInit();
             this.SuspendLayout();
-            // 
-            // picOggy
-            // 
-            this.picOggy.BackColor = System.Drawing.Color.Transparent;
-            this.picOggy.Location = new System.Drawing.Point(100, 460);
-            this.picOggy.Margin = new System.Windows.Forms.Padding(2);
-            this.picOggy.Name = "picOggy";
-            this.picOggy.Size = new System.Drawing.Size(175, 240);
-            this.picOggy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picOggy.TabIndex = 0;
-            this.picOggy.TabStop = false;
             // 
             // pnlInfo
             // 
@@ -52,12 +39,11 @@
             this.pnlInfo.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlInfo.Location = new System.Drawing.Point(0, 0);
             this.pnlInfo.Name = "pnlInfo";
-            this.pnlInfo.Size = new System.Drawing.Size(1234, 40);
+            this.pnlInfo.Size = new System.Drawing.Size(1250, 40);
             this.pnlInfo.TabIndex = 1;
             // 
             // timer
             // 
-            this.timer.Enabled = true;
             this.timer.Interval = 150;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
@@ -65,23 +51,19 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1234, 729);
+            this.ClientSize = new System.Drawing.Size(1250, 749);
             this.Controls.Add(this.pnlInfo);
-            this.Controls.Add(this.picOggy);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)(this.picOggy)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox picOggy;
         private System.Windows.Forms.Panel pnlInfo;
         private System.Windows.Forms.Timer timer;
     }
